7321051811e1fd7a2ab7bdd3f4089fbab001a918
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-12 00:38:05 +0700

    Update add_product.dart

--- lib/screen/add_product.dart
+++ lib/screen/add_product.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:firebase_admin/controller/auth_controller.dart';
 import 'package:firebase_admin/controller/imagepick_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:async';
 
+import '../controller/product_controller.dart';
+
 class AddProduct extends StatefulWidget {
-  AddProduct({
+  const AddProduct({
     Key? key,
     this.id,
     this.name,
@@ -21,7 +21,7 @@ class AddProduct extends StatefulWidget {
   }) : super(key: key);
   @override
   State<AddProduct> createState() => _AddProductState();
-  String? id;
+  final String? id;
   final String? name;
   final String? cat;
   final String? price;
@@ -39,11 +39,7 @@ class AddProduct extends StatefulWidget {
 class _AddProductState extends State<AddProduct> {
   Uint8List? image;
   File? file;
-  final proID = TextEditingController();
-  final proName = TextEditingController();
-  final proCate = TextEditingController();
-  final proPrice = TextEditingController();
-  final proDis = TextEditingController();
+
   selectImage() async {
     Uint8List im = await imagePick(ImageSource.gallery);
     setState(() {
@@ -60,175 +56,197 @@ class _AddProductState extends State<AddProduct> {
 
   @override
   Widget build(BuildContext context) {
-    final AuthController authController = Get.put(AuthController());
+    // final AuthController authController = Get.put(AuthController());
+    final productController = Get.put(ProductController());
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Add Product"),
+        title: Text(
+          widget.id != null ? 'Update Product' : "Add product",
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Stack(
-              children: [
-                const CircleAvatar(
-                  //backgroundColor: Colors.white,
-                  radius: 100,
-                  child: Icon(
-                    Icons.image,
-                    size: 50,
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Stack(
+                children: [
+                  const CircleAvatar(
+                    //backgroundColor: Colors.white,
+                    radius: 100,
+                    child: Icon(
+                      Icons.image,
+                      size: 50,
+                    ),
                   ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  left: 150,
-                  child: IconButton(
-                    onPressed: selectFile,
-                    icon: const Icon(Icons.add_a_photo),
+                  Positioned(
+                    bottom: 20,
+                    left: 150,
+                    child: IconButton(
+                      onPressed: selectFile,
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: proID,
-              decoration: InputDecoration(
-                hintText: "Product ID",
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                  borderSide: const BorderSide(
-                    color: Colors.black,
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                onSaved: (value) {
+                  productController.proid(value);
+                },
+                controller: productController.proID.value,
+                decoration: InputDecoration(
+                  hintText: "Product ID",
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: proName,
-              decoration: InputDecoration(
-                hintText: "Product name",
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                  borderSide: const BorderSide(
-                    color: Colors.black,
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                onSaved: (value) {
+                  productController.proname(value);
+                },
+                controller: productController.proName.value,
+                decoration: InputDecoration(
+                  hintText: "Product name",
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: proCate,
-              decoration: InputDecoration(
-                hintText: "Category",
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                  borderSide: const BorderSide(
-                    color: Colors.black,
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                onSaved: (value) {
+                  productController.category(value);
+                },
+                controller: productController.proCate.value,
+                decoration: InputDecoration(
+                  hintText: "Category",
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: proPrice,
-              decoration: InputDecoration(
-                hintText: "Price",
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                  borderSide: const BorderSide(
-                    color: Colors.black,
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                onSaved: (value) {
+                  productController.price(value);
+                },
+                controller: productController.proPrice.value,
+                decoration: InputDecoration(
+                  hintText: "Price",
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: proDis,
-              decoration: InputDecoration(
-                hintText: "Discount",
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                  borderSide: const BorderSide(
-                    color: Colors.black,
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                onSaved: (value) {
+                  productController.discount(value);
+                },
+                controller: productController.proDis.value,
+                decoration: InputDecoration(
+                  hintText: "Discount",
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final addProduct = AddProduct(
-                  id: proID.text,
-                  name: proName.text,
-                  cat: proCate.text,
-                  price: proPrice.text,
-                  discount: proDis.text,
-                );
-                addNew(addProduct);
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Add product",
-                  style: TextStyle(fontSize: 18),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  formKey.currentState != null
+                      ? formKey.currentState!.save()
+                      : null;
+                  widget.id != null
+                      ? productController.editProduct(widget.id!).then((value) {
+                          Navigator.pop(context);
+                          Get.snackbar('Success', 'Product Update successfully',
+                              backgroundColor: Colors.green);
+                        })
+                      : productController.addProduct().then((value) {
+                          Navigator.pop(context);
+                          Get.snackbar('Success', 'Product Add successfully',
+                              backgroundColor: Colors.green);
+                        });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    widget.id != null ? 'Update Product' : "Add product",
+                    style: const TextStyle(fontSize: 18),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Future addNew(AddProduct addProduct) async {
-  final docProduct = FirebaseFirestore.instance.collection('products').doc();
 
-  addProduct.id = docProduct.id;
-
-  final json = addProduct.toJson();
-  await docProduct.set(json);
-}
 
 
 
