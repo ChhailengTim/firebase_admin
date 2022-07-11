3dd21755e6887416179fe5c56f881b1c5e8e6d29
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-12 00:38:01 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
-import 'package:firebase_admin/controller/auth_controller.dart';
 import 'package:firebase_admin/screen/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+const _firebaseConfigWeb = FirebaseOptions(
+    apiKey: "AIzaSyCdrvlZPltji85BFFERgLgiS1mXRLoQ4do",
+    authDomain: "getx-admin.firebaseapp.com",
+    projectId: "getx-admin",
+    storageBucket: "getx-admin.appspot.com",
+    messagingSenderId: "401769481318",
+    appId: "1:401769481318:web:62dbc3cf92d7b7173f99a7");
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+  kIsWeb ? await Firebase.initializeApp(options: _firebaseConfigWeb) : null;
   runApp(const MyApp());
 }
 
