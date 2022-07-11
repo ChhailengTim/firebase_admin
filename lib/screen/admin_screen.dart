aed5b9a90f8f86fa32e8da01b86d7edc5ee8c401
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-11 19:30:37 +0700

    Update admin_screen.dart

--- lib/screen/admin_screen.dart
+++ lib/screen/admin_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_admin/controller/auth_controller.dart';
+import 'package:firebase_admin/screen/add_product.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,28 +11,41 @@ class AdminScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AuthController authController = Get.put(AuthController());
+
     return Scaffold(
-      appBar: AppBar(actions: [
-        IconButton(
+      appBar: AppBar(
+        leading: const CircleAvatar(
+          child: Icon(Icons.person),
+        ),
+        title: Text("$email"),
+        actions: [
+          IconButton(
             onPressed: () {
               authController.singout();
             },
-            icon: const Icon(Icons.logout_outlined)),
-      ]),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              child: Column(
-                children: [
-                  const Icon(Icons.people),
-                  Text('$email'),
-                ],
-              ),
-            ),
-          )
+            icon: const Icon(Icons.logout_outlined),
+          ),
         ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('products').snapshots(),
+        builder: (index, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("data");
+          }
+          return const Text("data");
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AddProduct(
+                        id: '',
+                      )));
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
